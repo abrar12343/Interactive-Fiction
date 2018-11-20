91d22abd90ce3bfaf25eb484a86b91d0737d0532
Deen17 <43797479+Deen17@users.noreply.github.com>
2018-11-19 22:29:06 -0500

    updated SET.
    this will prob be the way we do constructors in SectionType.h

--- SectionType.h
+++ SectionType.h
@@ -4,6 +4,7 @@
 #include "storytokenizer.h"
 #include <unordered_map>
 #include <utility>
+#include <string>
 using namespace std;
 
 unordered_map<string, bool> sets;
@@ -13,13 +14,29 @@ class Set : public SectionToken
 {
 private:
 	bool truth;
+	string name;
 public:
-
+	Set(string str) : SectionToken(str, SET)
+	{
+		
+		name = str.substr(str.find(SET_START, 0), str.find(" to ",0));
+		if(str.substr(str.find(" to ", 0), str.find(')', 0)) == "true")
+		{
+			truth = true;
+		}
+		else truth = false;
+	}
 };
 
 class Link : public SectionToken
 {
-	
+private:
+
+public:
+	Link(string str): SectionToken(str, LINK)
+	{
+		
+	}
 };
 
 class GoTo : public SectionToken
